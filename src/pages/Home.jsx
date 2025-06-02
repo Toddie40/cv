3ccf96d4fa5809c3f05f83c0eda928bb4185535b
Alex Todd <alex.todd40@gmail.com>
2025-06-02 22:53:41 +0100

    refactored the home jsx to be neater and added education component to the page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,15 @@
 import React from 'react';
-import Experience from '../components/Experience';
 import AboutMe from '../components/Aboutme';
-
-
-// Dynamically import all experience JSON files
-const experienceFiles = import.meta.glob('./experience/*.json', { eager: true });
-
-// Transform the imported files into an array of experience objects
-const experiences = Object.values(experienceFiles).map((module) => module.default);
+import Positions from '../components/Positions';
+import Education from '../components/Education';
 
 const Home = () => (
   <main className="max-w-4xl mx-auto py-8 space-y-8">
     
     <AboutMe />
-
-    {/* Dynamically render Experience components */}
-    <section className='experience p-4'>
-    <h2>Experience</h2>
-    {experiences.map((experience, index) => (
-      <Experience
-        title={experience.JobTitle}
-        date={experience.Date}
-        projects={experience.Projects}
-      />
-    ))}
-    </section>
+    <Positions />
+    <Education />
+    
   </main>
 );
 
