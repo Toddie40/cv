9ecd3f732ce6c740b4bf9e65a406a5512316ae8e
Alex Todd <alex.todd40@gmail.com>
2025-06-03 21:57:39 +0100

    added custom page font support. Now using NotoSans

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,12 @@ const AppContent = () => {
 
   return (
     <Router basename="/cv/"> {/* Set the base path */}
-      <div className="font-sans flex flex-col h-screen overflow-hidden">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/> 
+      </head>
+      <div className="page-font flex flex-col h-screen overflow-hidden">
         <Navbar/>
         <div className="overflow-y-scroll">
           <Routes>
