08ef1c5e229d1d771fe64cb8797adc87e20cdfd4
Alex Todd <Alex.Todd@uk.bmt.org>
2025-07-07 16:09:09 +0100

    Included the githubPagesSPA plugin to fix 404 errors when hosted in github pages.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+// Plugin for fixing 404 errors when hosting SPAs on github pages
+import { githubPagesSpa } from "@sctg/vite-plugin-github-pages-spa";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    githubPagesSpa({verbose: false})
   ],
   base: "/cv/"
 })
